6a63fedc33efacd84c80fa282cd2de0385501abe
JeanetteNgo <jeanettengo@u.nus.edu>
2021-02-10 02:02:23 +0800

    Homepage layout
    Added homepage gradient background, edited homepage layout. Improved overall aesthetics.

--- main.js/main.js
+++
@@ -1,9 +0,0 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('navLink');
-
-    burger.addEventListener('click',()=>{
-        nav.classList.toggle('.navActive')
-    });
-}
-navSlide();
