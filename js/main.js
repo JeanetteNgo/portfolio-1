663b12dd16b393d69b6ac5dd7cd9ccf2d8a3ba6a
JeanetteNgo <jeanettengo@u.nus.edu>
2021-04-28 13:19:57 +0800

    Added Portfolio Presentation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,17 @@
-// function toggleNav() {
-//     const topNav = document.getElementById("top-nav");
-//     if (topNav.className === "menu") {
-//         topNav.className += " responsive";
-//         topNav.style.width = "100%";
-//     } else {
-//         topNav.className = "menu";
-//         topNav.style.width = "0%";
-//     }
 
-//   }
-
-// const topNav = document.getElementById("top-nav");
-//   let toggleNav = function () {
-
-//   };
-
-//   $('#click_advance').click(function(){
-//     $('#display_advance').toggle('1000');
-//     icon = $(this).find("i");
-//     icon.toggleClass("icon-circle-arrow-up icon-circle-arrow-down")
-//   })
-
-function toggleNav () {
-    const icon = document.getElementById("menu-icon");
-      if (icon.classList.contains("fa fa-bars")) {
-        icon.classList.remove("fa fa-bars");
-        icon.classList.add("fas fa-times");
-    } else {
-       icon.classList.remove("fas fa-times");
-       icon.classList.add("fas fa-times");
-    }
+// About tabs
+function openTab(event, tabName) {
+ 
+   var i, tabContent, tabLinks;
+   tabContent = document.getElementsByClassName("tab-content");
+   for (i = 0; i < tabContent.length; i++) {
+     tabContent[i].style.display = "none";
+   }
+   
+   tabLinks = document.getElementsByClassName("tab-links");
+   for (i = 0; i < tabLinks.length; i++) {
+     tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+   }
+   document.getElementById(tabName).style.display = "block";
+   evt.currentTarget.className += " active";
  }
- icon.onclick = toggleNav;
